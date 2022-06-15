5c43355776c65ff89bd8764f5dfaf04e7d5178af
Dewa Ade-onojobi <adewalade@gmail.com>
2022-06-15 23:09:33 +0100

    ch-start app setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul v-for="resource in learningResources" :key="resource.id"></ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            learningResources: [
+                {
+                    id: "official-guide",
+                    title: "Official Guide",
+                    description: "The official Vue.js learning guide",
+                    link: "https://veujs.org"
+                },
+                {
+                    id: "google",
+                    title: "Google",
+                    description: "Search engine for finding more learning resources on the web",
+                    link: "https://google.org"
+                }
+            ]
+        }
+    }
+}
+</script>
