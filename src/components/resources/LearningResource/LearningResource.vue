a5d124d2b7b3156369edc8e51ef1985c09676328
Dewa Ade-onojobi <adewalade@gmail.com>
2022-06-16 08:37:16 +0100

    ft-add styling to app

--- src/components/resources/LearningResource/LearningResource.vue
+++ src/components/resources/LearningResource/LearningResource.vue
@@ -3,7 +3,7 @@
         <div>
             <header>
                 <h3>{{ resource.title }}</h3>
-                <button>Delete</button>
+                <button class="btn btn-sm btn-danger">Delete</button>
             </header>
         </div>
         <p>{{ resource.description }}</p>
@@ -19,10 +19,12 @@ export default {
         resource: {
             type: Object,
             required: true,
-            default: {
-                title: "",
-                description: "",
-                link: ""
+            default() {
+                return {
+                    title: "",
+                    description: "",
+                    link: ""
+                }
             },
             validator (value) {
                 if (typeof value === "object") {
@@ -35,3 +37,37 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    li {
+      margin: 20px auto;
+      max-width: 40rem;
+    }
+
+    header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    h3 {
+      font-size: 1.25rem;
+      margin: 0.5rem 0;
+    }
+
+    p {
+      margin: 0.5rem 0;
+    }
+
+    a {
+      text-decoration: none;
+      color: #ce5c00;
+      cursor: pointer;
+    }
+
+    a:hover,
+    a:active {
+      color: #c89300;
+      cursor: pointer;
+    }
+</style>
