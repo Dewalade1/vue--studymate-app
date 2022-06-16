27437d8ecace392ca847152d5a0a6ecf77de15ce
Dewa Ade-onojobi <adewalade@gmail.com>
2022-06-16 07:57:18 +0100

    ch-finish app setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-    <ul v-for="resource in learningResources" :key="resource.id"></ul>
+    <ul>
+        <learning-resource  
+            v-for="resource in learningResources" 
+            :resource="resource"
+            :key="resource.id"
+        ></learning-resource>
+    </ul>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            learningResources: [
-                {
-                    id: "official-guide",
-                    title: "Official Guide",
-                    description: "The official Vue.js learning guide",
-                    link: "https://veujs.org"
-                },
-                {
-                    id: "google",
-                    title: "Google",
-                    description: "Search engine for finding more learning resources on the web",
-                    link: "https://google.org"
-                }
-            ]
-        }
+    import LearningResource from "./components/resources/LearningResource/LearningResource.vue";
+
+    export default {
+        data() {
+            return {
+                learningResources: [
+                    {
+                        id: "official-guide",
+                        title: "Official Guide",
+                        description: "The official Vue.js learning guide",
+                        link: "https://veujs.org"
+                    },
+                    {
+                        id: "google",
+                        title: "Google",
+                        description: "Search engine for finding more learning resources on the web",
+                        link: "https://google.org"
+                    }
+                ]
+            }
+        },
+        components: { LearningResource }
     }
-}
 </script>
