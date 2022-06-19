19216c6e852370d6dd85e7980b3569d9e3347707
Dewa Ade-onojobi <adewalade@gmail.com>
2022-06-19 15:50:05 +0100

    ft-add a footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
     <the-header title="StudyMate"></the-header>
     <the-resource></the-resource>
+    <the-footer></the-footer>
 </template>
 
 <script>
+    import TheFooter from './components/layouts/TheFooter/TheFooter.vue';
     import TheHeader from './components/layouts/TheHeader/TheHeader.vue';
     import TheResource from './components/resources/TheResource/TheResource.vue';
 
     export default {
-        components: { TheHeader, TheResource }
+        components: { TheHeader, TheResource, TheFooter }
     }
 </script>
 
